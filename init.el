911476e6e4e59881a0c3dc743eb8a42e0ce708eb
Martin Elias <martin.elias@streamunlimited.com>
2018-02-19 15:17:02 +0100

    Add packages and move stuff

--- init.el
+++ init.el
@@ -13,19 +13,6 @@
 (eval-when-compile
   (require 'use-package))
 
-(use-package company
-  :ensure t
-  :config
-  (add-hook 'after-init-hook 'global-company-mode))
-
-(use-package highlight-parentheses
-  :ensure t
-  :init (global-highlight-parentheses-mode 1))
-
-(defalias 'yes-or-no-p 'y-or-n-p)
-(electric-pair-mode 1)
-(show-paren-mode 1)
-
 (let ((file-name-handler-alist nil))
   (add-to-list 'load-path "~/.emacs.d/lisp")
   (require 'm-custom)
@@ -36,4 +23,6 @@
   (require 'm-fonts)
   (require 'm-ivy)
   (require 'm-project)
-  (require 'm-go))
+  (require 'm-go)
+  (require 'm-json)
+  (require 'm-stuff))
---
+++ lisp/m-stuff.el
@@ -0,0 +1,27 @@
+;; Stuff that belongs nowhere for now.
+
+(use-package company
+  :ensure t
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
+
+(use-package highlight-parentheses
+  :ensure t
+  :config (global-highlight-parentheses-mode 1))
+
+(use-package git-gutter-fringe
+  :ensure t
+  :config (global-git-gutter-mode t))
+
+(use-package magit
+  :ensure t)
+
+(use-package editorconfig
+  :ensure t
+  :config (editorconfig-mode 1))
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+(electric-pair-mode 1)
+(show-paren-mode 1)
+
+(provide 'm-stuff)
