d30f70b073f9f3cc90acbe179afcb388b5a21098
Martin Elias <martin.elias@streamunlimited.com>
2018-03-14 14:18:52 +0100

    add protobuf and dumb-jump

--- lisp/m-stuff.el
+++ lisp/m-stuff.el
@@ -32,5 +32,15 @@
 (use-package cmake-mode
 	:ensure t)
 
+(use-package protobuf-mode
+	:ensure t)
+
+(use-package dumb-jump
+	:ensure t)
+
+(setq dumb-jump-selector 'ivy)
+;; (setq dumb-jump-force-searcher 'ag)
+;; (setq dumb-jump-prefer-searcher 'rg)
+
 
 (provide 'm-stuff)
