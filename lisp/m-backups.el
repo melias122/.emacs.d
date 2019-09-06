7d386f1a251d2239a57eaf83121be12efb12874a
Martin Elias <melias122@gmail.com>
2019-09-06 16:21:07 +0200

    use-package files for backups

--- lisp/m-backups.el
+++ lisp/m-backups.el
@@ -1,8 +1,10 @@
-(setq backup-directory-alist `(("." . "~/.saves"))
-	backup-by-copying t
-	delete-old-versions t
-	kept-new-versions 6
-	kept-old-versions 2
-	version-control t)
+(use-package files
+  :custom
+  (backup-directory-alist `(("." . "~/.saves")))
+  (backup-by-copying t)
+  (delete-old-versions t)
+  (kept-new-versions 6)
+  (kept-old-versions 2)
+  (version-control t))
 
 (provide 'm-backups)
