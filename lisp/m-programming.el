fa8d57fcd52174dcb9140def224667afdaa5ee52
Martin Elias <melias122@gmail.com>
2019-09-06 15:52:49 +0200

    move flymd to markdown-mode

--- lisp/m-programming.el
+++ lisp/m-programming.el
@@ -13,13 +13,13 @@
 (use-package markdown-mode
   :ensure t
   :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
+  :mode ( ("README\\.md\\'" . gfm-mode)
           ("\\.md\\'" . markdown-mode)
           ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
-
-(use-package flymd
-  :ensure t)
+  :custom (markdown-command "multimarkdown")
+  :config
+  (use-package flymd
+    :ensure t))
 
 (use-package yaml-mode
   :ensure t)
