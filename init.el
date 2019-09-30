7b035931cecdcf51789c327a3e8d68600cb458aa
Martin Elias <martin.elias@streamunlimited.com>
2019-09-30 08:35:39 +0200

    add more package-archive's

--- init.el
+++ init.el
@@ -15,8 +15,9 @@
 ;; Initialize package
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-  '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (package-initialize)
 
 ;; Bootstrap use-package
