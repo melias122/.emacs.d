3d55a0ba8f2d58c6ac8350b49b827c51232e2543
Martin Eliáš <martin.elias@axonpro.sk>
2020-12-22 12:49:13 +0100

    try eglot with gopls

--- lisp/m-programming.el
+++ lisp/m-programming.el
@@ -112,6 +112,7 @@
 (use-package spinner)
 
 (use-package lsp-mode
+  :disabled
   :commands (lsp lsp-deferred)
   :custom
   (lsp-eldoc-render-all nil)
@@ -121,6 +122,7 @@
   (lsp-headerline-breadcrumb-enable nil))
 
 (use-package company-lsp
+  :disabled
   :commands company-lsp
   :custom
   (company-transformers nil)
@@ -132,15 +134,69 @@
 ;;
 (use-package go-mode
   :init
-  (defun m/go-mode-hooks ()
-    (add-hook 'before-save-hook 'lsp-format-buffer t t)
-    (add-hook 'before-save-hook 'lsp-organize-imports t t))
-  :hook ( (go-mode . lsp-deferred)
-          (go-mode . m/go-mode-hooks)))
+
+  ;; make project understand go modules
+  (require 'project)
+
+  (defun project-find-go-module (dir)
+    (when-let ((root (locate-dominating-file dir "go.mod")))
+      (cons 'go-module root)))
+
+  (cl-defmethod project-root ((project (head go-module)))
+    (cdr project))
+
+  (add-hook 'project-find-functions #'project-find-go-module)
+
+  ;; Optional: install eglot-format-buffer as a save hook.
+  ;; The depth of -10 places this before eglot's willSave notification,
+  ;; so that that notification reports the actual contents that will be saved.
+  (defun eglot-format-buffer-on-save ()
+    (add-hook 'before-save-hook #'eglot-format-buffer -10 t))
+  (add-hook 'go-mode-hook #'eglot-format-buffer-on-save)
+
+  ;; eglot-organize-imports is hopefully a temporary stopgap until
+  ;; https://github.com/joaotavora/eglot/issues/574 is addressed.
+  (defun eglot-organize-imports ()
+    "Offer to execute the source.organizeImports code action."
+    (interactive)
+    (unless (eglot--server-capable :codeActionProvider)
+      (eglot--error "Server can't execute code actions!"))
+    (let* ((server (eglot--current-server-or-lose))
+            (actions (jsonrpc-request
+                       server
+                       :textDocument/codeAction
+                       (list :textDocument (eglot--TextDocumentIdentifier))))
+            (action (cl-find-if
+                      (jsonrpc-lambda (&key kind &allow-other-keys)
+                        (string-equal kind "source.organizeImports" ))
+                      actions)))
+      (when action
+        (eglot--dcase action
+          (((Command) command arguments)
+            (eglot-execute-command server (intern command) arguments))
+          (((CodeAction) edit command)
+            (when edit (eglot--apply-workspace-edit edit))
+            (when command
+              (eglot--dbind ((Command) command arguments) command
+                (eglot-execute-command server (intern command) arguments))))))))
+
+  (defun eglot-organize-imports-on-save ()
+    (defun eglot-organize-imports-nosignal ()
+      "Run eglot-organize-imports, but demote errors to messages."
+      ;; Demote errors to work around
+      ;; https://github.com/joaotavora/eglot/issues/411#issuecomment-749305401
+      ;; so that we do not prevent subsequent save hooks from running
+      ;; if we encounter a spurious error.
+      (with-demoted-errors "Error: %s" (eglot-organize-imports)))
+    (add-hook 'before-save-hook #'eglot-organize-imports-on-save))
+
+  (add-hook 'go-mode-hook #'eglot-organize-imports-on-save)
+
+  :hook ( (go-mode . eglot-ensure)
+          (go-mode . eglot-format-buffer-on-save)))
 
 (use-package eglot
-  :disabled
-  :hook ((c-mode c++-mode go-mode) . (lambda () (eglot-ensure)))
+  :hook ((c-mode c++-mode) . (lambda () (eglot-ensure)))
   :custom (company-transformers nil))
 
 ;;
