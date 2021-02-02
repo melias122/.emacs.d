55259a453e836e5a697cb54acaf41f8ed20be251
Martin Eliáš <martin.elias@axonpro.sk>
2021-02-02 20:17:47 +0100

    move ivy-xref to m-ivy

--- lisp/m-ivy.el
+++ lisp/m-ivy.el
@@ -35,4 +35,18 @@
   (push "vendor" projectile-globally-ignored-directories)
   (counsel-projectile-mode 1))
 
+(use-package ivy-xref
+  :init
+  ;; xref initialization is different in Emacs 27 - there are two different
+  ;; variables which can be set rather than just one
+  (when (>= emacs-major-version 27)
+    (setq xref-show-definitions-function #'ivy-xref-show-defs))
+  ;; Necessary in Emacs <27. In Emacs 27 it will affect all xref-based
+  ;; commands other than xref-find-definitions (e.g. project-find-regexp)
+  ;; as well
+  (setq xref-show-xrefs-function #'ivy-xref-show-xrefs)
+  :custom
+  (xref-prompt-for-identifier nil)
+  (xref-search-program 'ripgrep))
+
 (provide 'm-ivy)
