b8c3521d7dd1b7d5f1f5c44732eb0ea8916d303e
Martin Elias <melias122@gmail.com>
2022-04-11 13:06:01 +0200

    add consult-project-extra

--- lisp/m-projects.el
+++ lisp/m-projects.el
@@ -1,9 +1,16 @@
 (use-package project
   :bind-keymap ("C-c p" . project-prefix-map)
-  :bind ("C-k" . project-find-file)
   :config
-  (add-to-list 'project-switch-commands '(magit-status "Magit" ?m)))
+  (setq project-switch-commands
+    (delete '(project-find-file "Find file" ) project-switch-commands))
+  (add-to-list 'project-switch-commands '(magit-status "Magit" ?m))
+  (add-to-list 'project-switch-commands '(consult-project-extra-find "Find file" ?f)))
 
+(use-package consult-project-extra
+  :bind (("C-k" . consult-project-extra-find)
+         ("C-c p f" . consult-project-extra-find)
+         ("C-c p o" . consult-project-extra-find-other-window)))
+ 
 (use-package magit
   :bind ("C-x g" . magit-status)
   :custom (magit-bind-magit-project-status nil))
