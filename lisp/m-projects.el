2bf95ca8f99bf12f28a08b4f1b90b692a28f5604
Martin Elias <melias122@gmail.com>
2022-12-13 09:47:45 +0100

    fix project magit-status

--- lisp/m-projects.el
+++ lisp/m-projects.el
@@ -1,11 +1,15 @@
+(use-package magit
+  :ensure t
+  :bind ("C-x g" . magit-status)
+  :custom (magit-bind-magit-project-status nil))
+
 (use-package project
   :bind-keymap ("C-c p" . project-prefix-map)
   :config
   (setq project-switch-commands
     (delete '(project-find-file "Find file") project-switch-commands))
-  (add-to-list 'project-switch-commands '(magit-status "Magit" ?m))
+  (add-to-list 'project-switch-commands '(magit-project-status "Magit" ?m))
   (add-to-list 'project-switch-commands '(consult-project-extra-find "Find file" ?f)))
-
 
 (use-package consult-project-extra
   :ensure t
@@ -13,11 +17,6 @@
          :map project-prefix-map
          ("f" . consult-project-extra-find)
          ("o" . consult-project-extra-find-other-window)))
- 
-(use-package magit
-  :ensure t
-  :bind ("C-x g" . magit-status)
-  :custom (magit-bind-magit-project-status nil))
 
 (use-package diff-hl
   :ensure t
